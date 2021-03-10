408c4231f81ee402ba27606ce7e839f86cfb8fc4
juliana jones <juliana.p.jones@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email juliana.p.jones@gmail.com>
2021-03-10 13:15:14 -0500

    I just added my main file

---
+++ Main.java
@@ -0,0 +1,8 @@
+package testerscript;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I got my first bike in the hood from crackhead Billy");
+    }
+}
+///Users/julianajones/IdeaProjects/testproject/src/main/java/testerscript/Main.java
